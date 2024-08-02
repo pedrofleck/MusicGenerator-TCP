df2d40363602e793e4a0769614a0af5b5eceb69c
T-marti <tmdsilva@inf.ufrgs.br>
2024-08-02 14:28:53 -0300

    Criando tela

---
+++ general/Interface/src/BookEditor.java
@@ -0,0 +1,14 @@
+package general.Interface.src;
+
+import javax.swing.*;
+
+public class BookEditor {
+
+    private JRadioButton clicarRadioButton;
+    private JFormattedTextField parte1FormattedTextField;
+    private JPanel Telainicial;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
